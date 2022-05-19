bcb2866eacafaed2f6dbe427638cca3ff98ae453
Quinn Myers <quinnmyers@Quinns-MacBook-Pro.local>
2022-05-18 20:00:23 -0700

    improved ppp checker default styling

--- src/components/pppFallback/pppFallback.tsx
+++ src/components/pppFallback/pppFallback.tsx
@@ -1,4 +1,5 @@
 import Button from '../../components/button/button'
+import pegLogo from '../../images/peg_logo.svg'
 
 type PppFallbackProps = {
 
@@ -6,10 +7,11 @@ type PppFallbackProps = {
 
 export default function PppFallback(props: PppFallbackProps) {
   return (
-    <div className="flex flex-col w-full h-full items-center justify-center border border-primary rounded rounded-xl">
-      <div className="flex flex-col justify-center items-center content-center">
-        <h3 className='text-[1.3rem] mb-1'>Peg Pro Account Feature</h3>
-        <h4 className='mb-[3.5rem] text-[.9rem]'>In order to use this feature of Peg you must upgrade your account and purchase a license.</h4>
+    <div className="flex flex-col w-full h-full items-center justify-center border border-base-300 bg-base-200 rounded rounded-xl">
+      <div className="flex flex-col justify-center items-center content-center  bg-base-100 p-[4rem] rounded rounded-xl relative">
+        <img src={pegLogo} className='w-[12.5rem] mb-8' alt="peg logo" />
+        <h3 className='text-[1.2rem] mb-1'>Peg Pro Account Feature</h3>
+        <h4 className='mb-[3.5rem] text-[.9rem] max-w-[26rem] text-center'>In order to use this feature of Peg you must upgrade your account and purchase a license.</h4>
         <Button className='btn-outline btn-success relative animate-bounce' selected={true} onClick={() => { }}>
           {/* <span class="flex absolute -right-[0.2rem] -top-[0.2rem] h-3 w-3">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-success opacity-75"></span>
@@ -21,7 +23,7 @@ export default function PppFallback(props: PppFallbackProps) {
         <p className='text-[.75rem] mt-[1rem]'>To learn more about our pricing, features, and licenses please visit <span className='text-primary'>peg.software</span>.</p>
       </div>
 
-    </div>
+    </div >
   )
 }
 
