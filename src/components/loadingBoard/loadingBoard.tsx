8d26230b568684c6f855ce1ff8b159ebba45fb56
Quinn Myers <quinnmyers@Quinns-MacBook-Pro.local>
2022-05-19 21:51:24 -0700

    improved loading base styles

--- src/components/loadingBoard/loadingBoard.tsx
+++ src/components/loadingBoard/loadingBoard.tsx
@@ -54,24 +54,29 @@ export default function LoadingBoard(props: LoadingBoardProps) {
     const returnCorrectView = () => {
         if (!haveMap()) {
             return (
-                <div class="LoadingBoard flex flex-col w-full h-full justify-center relative">
-                    <div className="flex flex-col items-center">
-                        <img class='flex h-[11rem] mb-[1rem] self-center' src={pegSquareLogo} alt="peg application logo" />
-                        {/* ****** this Show handles the 'scan again' features of the old loading page fallback, so if I understand correct the show should be true while the app is successfully scanning, and the show should be False when the app is not scanning and we also do not have a layout. The user can then make the app scan again.  */}
-                        <Show when={!isScanning()} fallback={
-                            <h4 className='mb-[3rem] animate-pulse text-base-300'>Scanning...</h4>
-                        } >
-                            <div className="flex flex-col bg-red-200 justify-center items-center">
-                                <p className='text-[.75rem] max-w-[30rem]'><span className='text-warning'>Oops!</span> We found something while scanning but we aren't able to display the board. Please check your connected drives or visit <span className='text-primary'>peg.software/help</span> for more information.</p>
-                                <Button className={`btn-warning animate-bounce btn-xs mt-3`} onClick={scanAgain} selected={true} >
-                                    Scan Again
-                                </Button>
-                            </div>
+                <div class="LoadingBoard flex flex-col w-full h-full content-center relative">
+                    <div className="flex flex-col">
+                        <div className="flex flex-col mt-[7rem]">
+                            <img class='flex h-[11rem] mb-[1rem]' src={pegSquareLogo} alt="peg application logo" />
+                            <Show when={!isScanning()} fallback={
+                                <h4 className='mb-[3rem] animate-pulse text-base-300 self-center'>Scanning...</h4>
+                            } >
+                                <div className="flex flex-col justify-center items-center">
+                                    <p className='text-[.75rem] max-w-[27rem] text-center mb-2'><span className='text-warning'>Oops!</span> We found something while scanning but we aren't able to display the board. Please check your connected drives or visit <span className='text-primary'>peg.software/help</span> for more information.</p>
+                                    <Button className={`btn-warning animate-bounce btn-xs mt-3`} onClick={scanAgain} selected={false} >
+                                        Scan Again
+                                    </Button>
+                                </div>
+                            </Show>
+                        </div>
 
-                        </Show>
-                        <Show when={showSetup()} fallback="" >
-                            <NewBoardSetup />
-                        </Show>
+                        <div className="absolute bottom-0 w-full">
+                            {/* <Show when={showSetup()} fallback="" > */}
+                            <Show when={true} fallback="" >
+                                <NewBoardSetup />
+                            </Show>
+                        </div>
+
 
                     </div>
                 </div>
--- src/components/newBoardSetup/newBoardSetup.tsx
+++ src/components/newBoardSetup/newBoardSetup.tsx
@@ -35,13 +35,13 @@ export default function NewBoardSetup(props: NewBoardSetupProps) {
         <div className="NewBoardSetup flex flex-col w-full h-full justify-center relative">
 
 
-            <div className="newboard flex flex-col items-center self-center absolute bottom-[.5rem]">
-                <h2 className='text-[.9rem]'>Board Not Appearing?</h2>
-                <p className='text-[.8rem] mb-2'>You may need to configure your board to work with Peg.</p>
-                <Button className={`btn-success btn-xs mt-1 mb-3`} onClick={openModal} selected={true}  >
+            <div className="newboard flex flex-col items-center self-center absolute bottom-0">
+                <h2 className='text-[1rem] mb-1'>Board Not Appearing?</h2>
+                <p className='text-[.75rem] mb-3'>You may need to configure your board to work with Peg.</p>
+                <Button className={`btn-success btn-outline btn-xs mt-1 mb-[2.2rem]`} onClick={openModal} selected={true}  >
                     Setup New Board
                 </Button>
-                <p className='text-[.75rem]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam dolorum rerum, iste  ut modi.</p>
+                <p className='text-[.75rem]'>Visit <a href="#" className='link link-primary' target='_blank'>peg.software</a> for help setting up a new board.</p>
                 {/* ******* all of this stuff open in modal? ****** good way to fit it all OR we can have it replace the animation for scanning that is also a good option and keeps it all on page ****** */}
 
             </div>
